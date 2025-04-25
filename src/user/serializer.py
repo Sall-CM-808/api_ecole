4fcb3172aca1441c17403ff44218ee17951d81b4
Godecode62 <kikoyolo63@gmail.com>
2025-04-25 15:58:37 +0200

    Apres l'ajout de la fonction du mot de pass

--- src/user/serializer.py
+++ src/user/serializer.py
@@ -7,12 +7,16 @@ class UserCreationSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username','first_name','last_name','email','date_of_birth','address','profile_picture','role')
     
-    def rand_char(self):
-        char = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
-        number = [1,2,3,4,5,6,7,8,9]
-        special_char = ['&',"'","œ","é","(","-","_","è","ç",""],
-    
-    
+    #Definition de la fonction qui geneera un mot de pass pour l'utilisateur lors de la creation de son compte
+    def generate_password(password_size):
+        import string
+        import secrets
+        safe_punctuation = "!@#$%&*+-=?"
+        chars = string.ascii_letters + string.digits + safe_punctuation
+        return "".join(secrets.choice(chars) for _ in range(password_size))
+
+    print(generate_password(15))
+        
     def create(self, validated_data):
         #Avec ca on recupere les attributs des la class user le password y compris
         password = self.initial_data.get('password',"hadi")
